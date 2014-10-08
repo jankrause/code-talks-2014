3328004af7e87f6b6bd36b0ad1024b54b270a635
Jan Christian Krause <jakr@akra.de>
2014-10-08 18:40:37 +0200

    Add html-table to javadoc

--- src/main/java/de/jankrause/codetalks2014/CustomerService.java
+++ src/main/java/de/jankrause/codetalks2014/CustomerService.java
@@ -10,6 +10,56 @@ import de.jankrause.codetalks2014.exceptions.SystemException;
 public interface CustomerService {
 
 	/**
+	 * Accepts the given order by e-mail. The e-mail is send to the mail-address
+	 * of the order's {@link Customer} by using the configured SMTP-Server
+	 * 
+	 * <h3>Requirements:</h3>
+	 * 
+	 * <ul>
+	 * <li>The SMTP-server is configured in config-file
+	 * <b>systems.properties</b>.</li>
+	 * <li>The contents of the e-mail are defined in the template
+	 * <b>rejection.tpl</b>.</li>
+	 * </ul>
+	 * 
+	 * @param order What is rejected. Must not be null and contain a valid 
+	 *  {@link Customer} with a valid mail-address.
+	 * 
+	 * @throws SystemException
+	 *   If one of the following conditions is met:
+	 *   <table>
+	 *             <tr>
+	 *             <td><b>Code</b></td>
+	 *             <td><b>Condition, which causes the exception</b></td>
+	 *             </tr>
+	 *             <tr>
+	 *             <td>01</td>
+	 *             <td>If given order is null.</td>
+	 *             </tr>
+	 *             <tr>
+	 *             <td>02</td>
+	 *             <td>If the volume providing the template-files is not
+	 *             available.</td>
+	 *             </tr>
+	 *             <tr>
+	 *             <td>12</td>
+	 *             <td>If the given customer or his or her mail-address is null
+	 *             or invalid.</td>
+	 *             </tr>
+	 *             <tr>
+	 *             <td>17</td>
+	 *             <td>If the configured SMTP-Server is not available.</td>
+	 *             </tr>
+	 *             <tr>
+	 *             <td>18</td>
+	 *             <td>If the template rejection.tpl is not available.</td>
+	 *             </tr>
+	 *             </table>
+	 */
+	public void sendOrderAcceptance(Order order) throws SystemException;
+
+	
+	/**
 	 * Rejects the given order by e-mail. The e-mail is send to the mail-address of the order's 
 	 * {@link Customer} by using the configured SMTP-Server  
 	 * 
@@ -23,20 +73,13 @@ public interface CustomerService {
 	 * 
 	 * @throws SystemException If one of the following conditions is met:
 	 * 
-	 * |Code  |Condition, which causes the exception111                              | 
+	 * |Code  |Condition, which causes the exception                                 | 
 	 * |:-----|:---------------------------------------------------------------------| 
      * | 01   | If given order is null.                                              |
      * | 02   | If the volume providing the template-files is not available.         |
      * | 12   | If the given customer or his or her mail-address is null or invalid. |
      * | 17   | If the configured SMTP-Server is not available.                      |
-     * | 18   | If the template rejection.tpl is not available.                      |  
-	 * 
-	 * ABC       DEF
-	 * =======   ===================
- 	 * sd        ssss
-	 * sdsd      ssfsf
-	 * -------   -------------------
-	 *
+     * | 18   | If the template rejection.tpl is not available.                      |
 	 */
 	public void sendOrderRejection(Order order) throws SystemException;
 	
